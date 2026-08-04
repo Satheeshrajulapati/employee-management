cd40ec49553370f18cba8e41994d940e24517fae
Satheesh Rajulapati <satheesh.rajulapati@gmail.com>
2026-08-04 23:04:50 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/satheesh/employee/management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satheesh.employee.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
